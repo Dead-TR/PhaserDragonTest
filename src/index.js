74fe4d0a6c5e41d2d8c90297a13bd0bac321be93
DTR <t.terry.rice@gmail.com>
2020-08-27 21:11:19 +0300

    add enemy, and collision it with fireBall

--- src/index.js
+++ src/index.js
@@ -24,16 +24,12 @@ export const config = {
 };
 
 const game = new Phaser.Game(config);
-let green, player, cursors, shadow, fire;
+let green, player, cursors, shadow, fire, ships;
 let cCounter = 1000;
 let cDown = true;
 
 function preload (){
   this.load.image('green', './assets/img/green.gif');
-  this.load.image('gg_left', './assets/img/gg/pl.deadtr-left.gif');
-  this.load.image('gg_right', './assets/img/gg/pl.deadtr-right.gif');
-  this.load.image('gg_top', './assets/img/gg/pl.deadtr-top.gif');
-  this.load.image('gg_bottom', './assets/img/gg/pl.deadtr-bottom.gif');
 
   this.load.spritesheet(
     'dragonDown',
@@ -76,7 +72,6 @@ function preload (){
       frameHeight: 172,
     }
   );
-
   this.load.spritesheet(
     'fireball',
     './assets/img/gg/fireBall/fireball.png',
@@ -85,12 +80,21 @@ function preload (){
       frameHeight: 124,
     }
   );
+
+  this.load.image('ship_01', './assets/img/ship/ship(1).png');
+  this.load.image('ship_02', './assets/img/ship/ship(2).png');
+  this.load.image('ship_03', './assets/img/ship/ship(3).png');
+  this.load.image('ship_04', './assets/img/ship/ship(4).png');
+  this.load.image('ship_05', './assets/img/ship/ship(5).png');
 }
 
 function create (){
 
   green = this.add.tileSprite(0, 0, config.width, config.height, 'green').setOrigin(0, 0);
-  player = this.physics.add.sprite(400, 300, 'dragonDown').setOrigin(0.5);
+  player = this.physics.add
+    .sprite(400, 300, 'dragonDown')
+    .setOrigin(0.5)
+    .setDepth(1);
   // shadow = this.add.ellipse( //-- створює еліпс
   //   player.x,
   //   player.y+200,
@@ -104,10 +108,19 @@ function create (){
   shadow = this.physics.add.sprite(400, 300, 'dragonDown')
     .setOrigin(0.5)
     .setScale(0.6)
+    .setDepth(0)
     .setTint(0x000000)
     .setAlpha(0.5);
 
   fire = this.physics.add.group();
+  ships = this.physics.add.group();
+
+  ships.create(800, 200, 'ship_01')
+    .setOrigin(0.5)
+    .setScale(0.3)
+    .setDepth(1)
+    .setVelocity(0, -100)
+    .flipCounter = 0;
 
   /*
     Анімація з одного кадру
@@ -220,6 +233,9 @@ function create (){
   player.anims.play('down', true);
   shadow.anims.play('down', true);
   cursors = this.input.keyboard.createCursorKeys();
+
+  this.physics.add
+    .overlap(ships, fire, fireDie, null, this);
 }
 
 function update() {
@@ -234,7 +250,7 @@ function update() {
   shadow.y = player.y+300;
 
   if (cursors.left.isDown) {
-    player.setVelocityX(-100);
+    player.setVelocityX(-150);
     player.setVelocityY(0);
     player.anims.play('left', true);
 
@@ -243,7 +259,7 @@ function update() {
     shadow.scaleX = 0.5;
   }
   else if (cursors.right.isDown) {
-    player.setVelocityX(+100);
+    player.setVelocityX(+150);
     player.setVelocityY(0);
     player.anims.play('right', true);
 
@@ -252,7 +268,7 @@ function update() {
     shadow.scaleX = 0.5;
   }
   else if (cursors.up.isDown) {
-    player.setVelocityY(-100);
+    player.setVelocityY(-150);
     player.setVelocityX(0);
     player.anims.play('up', true);
 
@@ -262,7 +278,7 @@ function update() {
     shadow.anims.currentFrame = player.anims.currentFrame;
   }
   else if (cursors.down.isDown) {
-    player.setVelocityY(+100);
+    player.setVelocityY(+150);
     player.setVelocityX(0);
     console.log("update -> player", player)
     player.anims.play('down', true);
@@ -281,37 +297,101 @@ function update() {
     cDown = false;
 
     if (player.anims.currentAnim.key === 'down') {
-      createFireBall(0, 90, -90, 0, 100)
+      createFireBall(0, 90, -90, 0, 200)
     } else if (player.anims.currentAnim.key === 'up') {
-      createFireBall(0, -90, 90, 0, -100)
+      createFireBall(0, -90, 90, 0, -200)
     } else if (player.anims.currentAnim.key === 'right') {
-      createFireBall(90, 50, 180, 100, 0)
+      createFireBall(90, 50, 180, 200, 0)
     } else if (player.anims.currentAnim.key === 'left') {
-      createFireBall(-90, 50, 0, -100, 0)
+      createFireBall(-90, 50, 0, -200, 0)
     }
   }
 
+  shipGoing.bind(this)(ships);
+
+}
+
+const fireDie = () => {
+  fire.children.entries[0]
+    .anims.play('fireDie', true)
+    .on(
+      'animationcomplete',
+      () => {
+        if (fire.children.entries[0]) {
+          fire.children.entries[0].destroy()
+        }
+      },
+      this
+    );
 }
 
 function createFireBall(addX, addY, angle, velocityX, velocityY) {
   fire.create(player.x + addX, player.y + addY, 'fireball')
-      .setScale(0.2)
+    .setScale(0.2)
+    .setDepth(2)
+    .setOrigin(0.5)
+    .anims.play('fireBall', true)
+    .setAngle(angle)
+    .setVelocity(velocityX, velocityY).on(
+      'animationcomplete',
+      fireDie,
+      this
+    );
+}
+
+function shipGoing(ships) {
+  const rotate = (ship, X, Y) => {
+    ship.setVelocity(X, Y);
+    if (ship.body.velocity.x === 100) {
+      ship.angle = 90;
+    } else if (ship.body.velocity.x === -100) {
+      ship.angle = -90;
+    } else if (ship.body.velocity.y === 100) {
+      ship.angle = 180;
+    } else if (ship.body.velocity.y === -100) {
+      ship.angle = 0;
+    }
+  }
+
+  const dist = (ship, coordinate, rotateX, rotateY) => {
+
+    ship.flipCounter++;
+    const canRotate = (
+      ship.flipCounter%10 == true
+      && Phaser.Math.Between(0, 15) === 0
+    )
+
+    if (ship.y < 50) {rotate(ship, 0, 100)}
+    if (ship.y > config.height -50) {rotate(ship, 0, -100)}
+
+    if (ship.x < 50) {rotate(ship, 100, 0)}
+    if (ship.x > config.width -50) {rotate(ship, -100, 0)}
+
+    if (canRotate) {
+      Phaser.Math.Between(0, 1) === 0
+        ? rotate(ship, rotateX, rotateY)
+        : rotate(ship, -rotateX, -rotateY);
+    }
+  }
+
+  for(const ship of ships.children.entries) {
+    if (ship.shadow === undefined) {
+      console.log("shipGoing -> ship.shadow", ship.shadow)
+
+      ship.shadow = this.physics.add.sprite(ship.x, ship.y, ship.texture.key)
       .setOrigin(0.5)
-      .anims.play('fireBall', true)
-      .setAngle(angle)
-      .setVelocity(velocityX, velocityY).on(
-        'animationcomplete',
-        () => {
-          fire.children.entries[0]
-            .anims.play('fireDie', true)
-            .on(
-              'animationcomplete',
-              () => {
-                fire.children.entries[0].destroy()
-              },
-              this
-            );
-        },
-        this
-      );
+      .setScale(ship.scale - 0.07)
+      .setTint(0x000000)
+      .setDepth(0)
+      .setAlpha(0.5);
+    } else {
+      ship.shadow.x = ship.x;
+      ship.shadow.y = ship.y+300;
+      ship.shadow.angle = ship.angle;
+    }
+    Phaser.Math.Between(0, 1) === 0
+        ? dist(ship, 'x', 100, 0)
+        : dist(ship, 'y', 0, 100);
+  }
+
 }
